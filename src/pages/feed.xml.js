bfe4555e4647494555720491c192f0cfa99aa139
redsh4de <25299353+redsh4de@users.noreply.github.com>
2025-11-27 11:25:10 +0200

    format

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,13 +1,13 @@
-import rss from '@astrojs/rss';
-import { getDateStringFromSlug } from '@/utils/blog';
-import { getCollection } from 'astro:content';
-import safeMarkdown from '@/utils/safeMarkdown';
+import rss from "@astrojs/rss";
+import { getDateStringFromSlug } from "@/utils/blog";
+import { getCollection } from "astro:content";
+import safeMarkdown from "@/utils/safeMarkdown";
 
 export async function GET(context) {
-  const blog = await getCollection('blog');
+  const blog = await getCollection("blog");
   return rss({
-    title: 'Monero',
-    description: 'Monero Blog RSS Feed',
+    title: "Monero",
+    description: "Monero Blog RSS Feed",
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
