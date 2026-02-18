d4611480b45338c8ad804f91ac8cf477238a2790
nahuhh <50635951+nahuhh@users.noreply.github.com>
2026-02-18 17:51:18 +0000

    downloads: use /downloads symlink for backward compatiblity (#141)
    * downloads: use /downloads symlink for backward compatiblity
    
    * create astro routes
    
    ---------
    
    Co-authored-by: redsh4de <25299353+redsh4de@users.noreply.github.com>

---
+++ src/pages/downloads/hashes.txt.ts
@@ -0,0 +1,9 @@
+import type { APIRoute } from "astro";
+import { readFileSync } from "node:fs";
+
+export const GET: APIRoute = () => {
+  const content = readFileSync("downloads/hashes.txt", "utf-8");
+  return new Response(content, {
+    headers: { "Content-Type": "text/plain" },
+  });
+};
